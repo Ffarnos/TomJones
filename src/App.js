e9a3755d88c9fe238fa532df3eb9cc3a29d4eba6
Ffarnos <ffarnos12@gmail.com>
2021-03-02 20:58:06 -0300

    update height of double image and sushi logo!

--- src/App.js
+++ src/App.js
@@ -12,14 +12,14 @@ function App() {
         <div className="App">
         <header className="App-header">
             <img src={headerImage} className="TomJonesLogo" alt="logo"/>
-            <img src={sushi} className="TomJonesLogo" alt={"logo"}/>
+            <img src={sushi} className="SushiLogo" alt={"logo"}/>
             <Link to="/menu" className="menuButton">MENÚ</Link>
             <Link to="/tragos" className="menuButton">TRAGOS</Link>
             <Link to="/sinalcohol" className="menuButton">SIN ALCOHOL</Link>
             <Link to="/botellas" className="menuButton">BOTELLAS</Link>
             <div className="menuDoubleImage">
                 <img src={heineken} className="menuDoubleImage" alt={"logo"}/>
-                <img src={branca} className="menuDoubleImage" alt="logo"/>
+                <img src={branca} className="menuDoubleImage2" alt="logo"/>
             </div>
         </header>
         </div>
