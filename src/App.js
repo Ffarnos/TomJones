1dbf597ab495aacebe404fbb1db00c73b4afe1b1
Ffarnos <ffarnos12@gmail.com>
2021-03-02 20:39:56 -0300

    Test new model web

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import headerImage from './assets/tomjones-background.png';
 import heineken from './assets/heineken-background.png';
 import sushi from './assets/sushi-background.png';
+import branca from './assets/branca.jpg';
+
 import './App.css';
 import {Link} from "react-router-dom";
 
@@ -10,13 +12,14 @@ function App() {
         <div className="App">
         <header className="App-header">
             <img src={headerImage} className="TomJonesLogo" alt="logo"/>
+            <img src={sushi} className="TomJonesLogo" alt={"logo"}/>
             <Link to="/menu" className="menuButton">MENÚ</Link>
             <Link to="/tragos" className="menuButton">TRAGOS</Link>
             <Link to="/sinalcohol" className="menuButton">SIN ALCOHOL</Link>
             <Link to="/botellas" className="menuButton">BOTELLAS</Link>
             <div className="menuDoubleImage">
                 <img src={heineken} className="menuDoubleImage" alt={"logo"}/>
-                <img src={sushi} className="menuDoubleImage" alt="logo"/>
+                <img src={branca} className="menuDoubleImage" alt="logo"/>
             </div>
         </header>
         </div>
