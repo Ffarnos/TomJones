e67aef86e8fd6cc1144c51a01fddb1a944d8133e
Ffarnos <ffarnos12@gmail.com>
2021-07-30 19:25:57 -0300

    Update web design

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import headerImage from './assets/tomjones-background.png';
+import apostoles from './assets/apostoles.png';
 import heineken from './assets/heineken-background.png';
-import sushi from './assets/sushi-background.png';
 import branca from './assets/branca.png';
 
 import './App.css';
@@ -12,15 +12,15 @@ function App() {
         <div className="App">
         <header className="App-header">
             <img src={headerImage} className="TomJonesLogo" alt="logo"/>
-            <img src={sushi} className="SushiLogo" alt={"logo"}/>
             <Link to="/menu" className="menuButton">MENÚ</Link>
             <Link to="/tragos" className="menuButton">TRAGOS</Link>
-            <Link to="/vinos" className="menuButton">VINOS</Link>
+            <Link to="/vinos" className="menuButton">VINOS Y ESPUMANTES</Link>
             <Link to="/sinalcohol" className="menuButton">SIN ALCOHOL</Link>
             <Link to="/botellas" className="menuButton">BOTELLAS</Link>
             <div className="menuDoubleImage">
-                <img src={heineken} className="menuDoubleImage" alt={"logo"}/>
-                <img src={branca} className="menuDoubleImage2" alt="logo"/>
+                <img src={apostoles} className="menuDoubleImageApostoles" alt={"logo"}/>
+                <img src={heineken} className="menuDoubleImageHeineken" alt={"logo"}/>
+                <img src={branca} className="menuDoubleImageBranca" alt="logo"/>
             </div>
         </header>
         </div>
