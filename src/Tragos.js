54631a93584f2424effa4beef4ee23264684560b
Ffarnos <ffarnos12@gmail.com>
2021-10-22 17:03:58 -0300

    Menu, Vinos, Tragos and sinalcohol update
    Vinos after 12 hour change

--- src/Tragos.js
+++ src/Tragos.js
@@ -2,10 +2,12 @@ import React from 'react';
 import './Menu.css'
 import headerImage from "./assets/tragos.jpg";
 
+
 function Tragos() {
     return (
         <img src={headerImage} className="Menu" alt="logo"/>
     );
+
 }
 
 export default Tragos;
--- src/Vinos.js
+++ src/Vinos.js
@@ -1,11 +1,24 @@
 import React from 'react';
 import './Menu.css'
 import headerImage from "./assets/vinos.jpg";
+import headerImageAfter12 from "./assets/vinos12.jpg";
+
 
 function Vinos() {
-    return (
-        <img src={headerImage} className="Menu" alt="logo"/>
-    );
+
+    var hour = new Date().getHours().toLocaleString("es-AR", {timeZone: "America/Argentina/Buenos_Aires"})
+
+    if (hour => 0 && hour <= 8) {
+        return (
+            <img src={headerImageAfter12} className="Menu" alt="logo"/>
+        );
+    }
+    else
+        return (
+            <img src={headerImage} className="Menu" alt="logo"/>
+        );
+
+
 }
 
 export default Vinos;
